0c071bbf6d32c39f80bf912423dcc22239d785b2
ChristianWBrock <christian.brock AT posteo.net>
2018-05-13 17:01:21 +0200

    Determine maxima arround Ha and plot in report

--- bin/generate_report.py
+++ bin/generate_report.py
@@ -16,11 +16,15 @@ from astropy import constants as const
 from astropy import units as u
 from astropy.coordinates import EarthLocation
 
+from astropy.modeling.functional_models import Gaussian1D
+
 from astropy.convolution import Box1DKernel
 from astropy.convolution import convolve
 
 from astropy.modeling.fitting import SimplexLSQFitter
 from reduction.nan_statistics import nan_leastsquare
+
+from scipy.optimize import curve_fit
 
 from argparse import ArgumentParser
 
@@ -40,7 +44,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-Diff = namedtuple('Diff', 'wavelength diff phase')
+Diff = namedtuple('Diff', 'wavelength diff phase maxima')
 
 
 def main():
@@ -49,7 +53,9 @@ def main():
 
     max_diff = 0.25
     padding = 10.0
-    continuum_ranges = closed_range(6520, H_ALPHA.value - padding) | closed_range(H_ALPHA.value + padding, 6610)
+
+    disc_range = closed_range(H_ALPHA.value - padding, H_ALPHA.value + padding)
+    continuum_ranges = closed_range(6520, 6610) & ~disc_range
 
     parser = ArgumentParser(parents=[filename_parser('spectrum'), verbose_parser],
                             description='Generate LaTeX report displaying spectrums normalized around the Halpha line.')
@@ -82,38 +88,46 @@ def main():
     observer_location = EarthLocation.from_geodetic(lon=15.0, lat=50.0)
     algol = Algol()
 
-    text_file = open(os.path.join(args.output, "report.tex"), "w")
+    tex_file = open(os.path.join(args.output, "report.tex"), "w")
 
-    text_file.write("\\documentclass{article}\n")
-    text_file.write("\\usepackage[utf8]{inputenc}\n")
-    text_file.write("\\usepackage{graphicx}\n")
-    text_file.write("\\usepackage{seqsplit}\n")
-    text_file.write("\\usepackage[hidelinks]{hyperref}\n")
-    text_file.write("\\title{Project Algol\\\\Spectrum reduction}\n")
-    text_file.write("\\date{\\today}\n")
-    text_file.write("\\author{%s\\\\by Christian Brock}\n" % os.path.basename(__file__).replace('_', '\\_'))
-    text_file.write("\\begin{document}\n")
-    text_file.write("\\maketitle\n")
-    text_file.write("\\begin{verbatim}\n")
+    tex_file.write("\\documentclass{article}\n")
+    tex_file.write("\\usepackage[utf8]{inputenc}\n")
+    tex_file.write("\\usepackage{graphicx}\n")
+    tex_file.write("\\usepackage{seqsplit}\n")
+    tex_file.write("\\usepackage{longtable}\n")
+    tex_file.write("\\usepackage[hidelinks]{hyperref}\n")
+    tex_file.write("\\title{Project Algol\\\\Spectrum reduction}\n")
+    tex_file.write("\\date{\\today}\n")
+    tex_file.write("\\author{%s\\\\by Christian Brock}\n" % os.path.basename(__file__).replace('_', '\\_'))
+    tex_file.write("\\begin{document}\n")
+    tex_file.write("\\maketitle\n")
+    tex_file.write("\\begin{verbatim}\n")
 
     for k,v in args.__dict__.items():
-        text_file.write("--%s %s\n" % (k, v))
+        tex_file.write("--%s %s\n" % (k, v))
 
-    text_file.write("\\end{verbatim}\n")
-    text_file.write("\\tableofcontents\n")
+    tex_file.write("\\end{verbatim}\n")
+    tex_file.write("\\tableofcontents\n")
 
     diff_image_name = "diff_by_phase.png"
+    diff_image_wm_name = "diff_by_phase_with_maxima.png"
     sorted_diff_image_name = "diff_sorted_phase.png"
-    text_file.write("\n")
-    text_file.write("\\section{Final Result}\n")
-    text_file.write("\n")
-    text_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % diff_image_name)
-    text_file.write("\n")
-    text_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % sorted_diff_image_name)
-    text_file.write("\n")
+    sorted_diff_image_wm_name = "diff_sorted_phase_with_maxima.png"
+
+    tex_file.write("\n")
+    tex_file.write("\\section{Final Result}\n")
+    tex_file.write("\n")
+    tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % diff_image_name)
+    tex_file.write("\n")
+    tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % diff_image_wm_name)
+    tex_file.write("\n")
+    tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % sorted_diff_image_name)
+    tex_file.write("\n")
+    tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % sorted_diff_image_wm_name)
+    tex_file.write("\n")
 
     # list of Diffs
-    diff_plots = []
+    diffs_by_phase = []
 
     filenames = poly_glob(args.filenames)
 
@@ -139,13 +153,13 @@ def main():
         logger.info("pass2 %d/%d: %s", n, len(spectra), spectrum.short_name)
 
         if spectrum.observer != prev_observer:
-            text_file.write("\section{%s}\n\n" % spectrum.observer)
+            tex_file.write("\section{%s}\n\n" % spectrum.observer)
             prev_observer = spectrum.observer
             prev_day = None
 
         obs_day = spectrum.obs_date.iso[:10]
         if obs_day != prev_day:
-            text_file.write("\subsection{%s}\n\n" % obs_day)
+            tex_file.write("\subsection{%s}\n\n" % obs_day)
             prev_day = obs_day
 
         xs = spectrum.xs
@@ -167,8 +181,8 @@ def main():
         rv_predicted = algol_rv_a - radial_velocity_correction
         phase = algol.AB.phase(obs_time)
 
-        def as_readshift(radial_velovity):
-            return H_ALPHA * (radial_velovity / const.c).to(1)
+        def as_redshift(radial_velocity):
+            return H_ALPHA * (radial_velocity / const.c).to(1)
 
         redshift_from_data = u.Quantity(_find_minimum(xs, ys, spectrum.dx, 10, 1.5), u.AA) - H_ALPHA
 
@@ -180,7 +194,7 @@ def main():
         initial_model.sigma.fixed = not args.fit_sigma
 
         normalized, snr = normalize(xs, ys, ref_ys=initial_model(xs), deg=args.deg, continuum_ranges=continuum_ranges,
-                               method=None, requested_plot=plt.figure().add_subplot(111))
+                                    method=None, requested_plot=plt.figure().add_subplot(111))
 
         image_norm1 = "%05d_norm1.png" % n
         plt.title("Normalization: %s" % initial_model)
@@ -198,7 +212,7 @@ def main():
             logger.debug("fit info: %s", fitter.fit_info)
 
             normalized, snr = normalize(xs, ys, ref_ys=final_model(xs), deg=args.deg, continuum_ranges=continuum_ranges,
-                                   method=None, requested_plot=plt.figure().add_subplot(111))
+                                        method=None, requested_plot=plt.figure().add_subplot(111))
 
             image_norm2 = "%05d_norm2.png" % n
             plt.title("Normalization: %s" % final_model)
@@ -214,121 +228,256 @@ def main():
         xlim[0] = max(xlim[0], continuum_ranges.lower_bound())
         xlim[1] = min(xlim[1], continuum_ranges.upper_bound())
 
-        create_diff_plot(final_model, initial_model, normalized, spectrum.short_name, xlim, xs, ys,
+        diff_xs = xs - final_model.redshift
+        diff_ys = normalized - final_model(xs)
+
+        diff_mask = [x in disc_range for x in diff_xs]
+
+        diff_xs = diff_xs[diff_mask]
+        diff_ys = diff_ys[diff_mask]
+
+        maxima = _find_maxima(diff_xs, diff_ys, H_ALPHA.value)
+
+        diffs_by_phase.append(Diff(diff_xs, diff_ys, phase, maxima))
+
+        create_diff_plot(final_model, initial_model, normalized, maxima, spectrum.short_name, xlim, xs, ys,
                          os.path.join(args.output, image_diff))
 
         def display(q, format_string):
             return ((format_string + " %s") % (q.value, q.unit)).replace('Angstrom', r'\AA')
 
         def display_rv(rv):
-            return r"%.1f km/s, %.2f \AA" % (rv.to('km/s').value, as_readshift(rv).to('AA').value)
+            return r"%.1f km/s, %.2f \AA" % (rv.to('km/s').value, as_redshift(rv).to('AA').value)
 
-        text_file.write("\n")
-        text_file.write("\\begin{center}\n")
-        text_file.write("\\begin{tabular}{|l|l|}\n")
-        text_file.write("\\hline\n")
-        text_file.write("Observer & %s \\\\\n" % spectrum.observer.replace('_', '\\_'))
-        text_file.write("Filename & \\seqsplit{%s} \\\\\n" % spectrum.short_name.replace('_', '\\_'))
-        text_file.write("\\hline\n")
-        text_file.write("Resolution $\\delta\\lambda/\\lambda$ & %s \\\\\n" % spectrum.resolution)
-        text_file.write("Sigma & %s \\\\\n" % display(sigma.to('AA'), "%.2f"))
-        text_file.write("SNR & %.0f \\\\\n" % snr)
-        text_file.write("\\hline\n")
-        text_file.write("Observation date $(UTC)$ & %s \\\\\n" % spectrum.obs_date.iso)
-        text_file.write("Light travel time& %s \\\\\n" % display(light_travel_time.to('min'), "%.1f"))
-        text_file.write("Phase & $%.2f$ \\\\\n" % phase)
-        text_file.write("\\hline\n")
-        text_file.write("Algol radial velocity & %s \\\\\n" % display_rv(algol_rv_a))
-        text_file.write("Barycentric correction & %s \\\\\n" % display_rv(radial_velocity_correction))
-        text_file.write("Final radial velocity& %s \\\\\n" % display_rv(rv_predicted))
-        text_file.write("\\hline\n")
-        text_file.write("Redshift, form data & %s \\\\\n" % display(redshift_from_data.to('AA'), "%.2f"))
-        text_file.write("\\hline\n")
-        text_file.write("\\end{tabular}\n")
-        text_file.write("\\end{center}\n")
+        tex_file.write("\n")
+        tex_file.write("\\begin{center}\n")
+        tex_file.write("\\begin{tabular}{|l|l|}\n")
+        tex_file.write("\\hline\n")
+        tex_file.write("Observer & %s \\\\\n" % spectrum.observer.replace('_', '\\_'))
+        tex_file.write("Filename & \\seqsplit{%s} \\\\\n" % spectrum.short_name.replace('_', '\\_'))
+        tex_file.write("\\hline\n")
+        tex_file.write("Resolution $\\delta\\lambda/\\lambda$ & %s \\\\\n" % spectrum.resolution)
+        tex_file.write("Sigma & %s \\\\\n" % display(sigma.to('AA'), "%.2f"))
+        tex_file.write("SNR & %.0f \\\\\n" % snr)
+        tex_file.write("\\hline\n")
+        tex_file.write("Observation date $(UTC)$ & %s \\\\\n" % spectrum.obs_date.iso)
+        tex_file.write("Light travel time& %s \\\\\n" % display(light_travel_time.to('min'), "%.1f"))
+        tex_file.write("Phase & $%.2f$ \\\\\n" % phase)
+        tex_file.write("\\hline\n")
+        tex_file.write("Algol radial velocity & %s \\\\\n" % display_rv(algol_rv_a))
+        tex_file.write("Barycentric correction & %s \\\\\n" % display_rv(radial_velocity_correction))
+        tex_file.write("Final radial velocity& %s \\\\\n" % display_rv(rv_predicted))
+        tex_file.write("\\hline\n")
+        tex_file.write("Redshift, form data & %s \\\\\n" % display(redshift_from_data.to('AA'), "%.2f"))
+        tex_file.write("\\hline\n")
+        tex_file.write("\\end{tabular}\n")
+        tex_file.write("\\end{center}\n")
 
-        text_file.write("\n")
-        text_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_diff)
-        text_file.write("\n")
-        text_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_norm1)
-        text_file.write("\n")
+        tex_file.write("\n")
+        tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_diff)
+        tex_file.write("\n")
+        tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_norm1)
+        tex_file.write("\n")
         if image_norm2:
-            text_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_norm2)
-            text_file.write("\n")
-        text_file.write("\\pagebreak\n")
-        text_file.write("\n")
-
-        diff_plots.append(Diff(xs - final_model.redshift, normalized - final_model(xs), phase))
+            tex_file.write("\\includegraphics[width=\\textwidth]{%s}\n" % image_norm2)
+            tex_file.write("\n")
+        tex_file.write("\\pagebreak\n")
+        tex_file.write("\n")
 
     # end spectra
 
-    vmin = max(-max_diff, np.min([np.nanmin(diff.diff) for diff in diff_plots]))
-    vmax = min(+max_diff, np.max([np.nanmax(diff.diff) for diff in diff_plots]))
+    diffs_by_phase = sorted(diffs_by_phase, key=lambda diff: diff.phase)
 
+    vmin = max(-max_diff, np.min([np.nanmin(diff.diff) for diff in diffs_by_phase]))
+    vmax = min(+max_diff, np.max([np.nanmax(diff.diff) for diff in diffs_by_phase]))
+
+    plot_diff(args.cmap, args.output, diff_image_name, diffs_by_phase, disc_range, vmin, vmax, False)
+    plot_diff(args.cmap, args.output, diff_image_wm_name, diffs_by_phase, disc_range, vmin, vmax, True)
+
+    plot_sorted_diff(args.cmap, args.output, sorted_diff_image_name, diffs_by_phase, disc_range, vmin, vmax, False)
+    plot_sorted_diff(args.cmap, args.output, sorted_diff_image_wm_name, diffs_by_phase, disc_range, vmin, vmax, True)
+
+    max_file = open(os.path.join(args.output, "maxima.dat"), "w")
+
+    tex_file.write("\\appendix\n")
+    tex_file.write("\\section{maxima of differences}\n")
+    tex_file.write("\n")
+    tex_file.write("The raw date is stored in {\\tt %s}\n" % "maxima.dat")
+    tex_file.write("\n")
+    tex_file.write("\\begin{longtable}{|l|ll|ll|}\n")
+    tex_file.write("\\hline\n")
+    tex_file.write("phase & x & y & x & y \\\\\n")
+    max_file.write("#phase,x1,y1,x2,y2\n")
+    tex_file.write("\\hline\n")
+
+    for diff in diffs_by_phase:
+
+        if len(diff.maxima) == 2:
+            x1, y1 = diff.maxima[0]
+            x2, y2 = diff.maxima[1]
+        elif len(diff.maxima) == 1:
+            x, y = diff.maxima[0]
+            if x < H_ALPHA.value:
+                x1, y1 = x, y
+                x2, y2 = None, None
+            else:
+                x1, y1 = None, None
+                x2, y2 = x, y
+
+        def _(value, fmt):
+            return fmt % value if value else ''
+
+        tex_file.write("%.5f & %s & %s & %s & %s\\\\\n" %
+                       (diff.phase, _(x1, '%.1f'), _(y1, '%.3f'), _(x2, '%.1f'), _(y2, '%.3f')))
+
+        max_file.write("%.5f,%s,%s,%s,%s\n" %
+                       (diff.phase, _(x1, '%.1f'), _(y1, '%.3f'), _(x2, '%.1f'), _(y2, '%.3f')))
+
+    tex_file.write("\\hline\n")
+    tex_file.write("\\end{longtable}\n")
+
+    tex_file.write("\\end{document}\n")
+
+    max_file.close()
+    tex_file.close()
+
+
+def plot_sorted_diff(args_cmap, args_output, sorted_diff_image_name, diffs_by_phase, disc_range, vmin, vmax, plot_maxima):
+    # create the trailed spectrum *sorted* by phase plot
     fig = plt.figure(figsize=[6.4, 4.8 * 2])
     plot = fig.add_subplot(111)
     plot.set_title("measured - predicted")
+    plot.set_xlim(disc_range.lower_bound(), disc_range.upper_bound())
+    plot.set_ylabel('Spectra sorted by phase')
 
+    sc = None
+
+    left_xs = []
+    left_ys = []
+    right_xs = []
+    right_ys = []
+
+    for i, diff in enumerate(diffs_by_phase):
+        assert len(diff.wavelength) == len(diff.diff)
+
+        ys = 1.0 * i * np.ones(len(diff.wavelength))
+        sc = plot.scatter(diff.wavelength, ys, s=1, c=diff.diff, cmap=args_cmap, vmin=min(vmin, -vmax),
+                          vmax=max(vmax, -vmin))
+
+        if 0.15 <= diff.phase <= 0.85:
+            for x, y in diff.maxima:
+                if x < H_ALPHA.value:
+                    left_xs.append(x)
+                    left_ys.append(i)
+                else:
+                    right_xs.append(x)
+                    right_ys.append(i)
+
+    plot.vlines(H_ALPHA.value, *plot.get_ylim())
+
+    if plot_maxima:
+        plot.plot(left_xs, left_ys, 'k')
+        plot.plot(right_xs, right_ys, 'k')
+
+    fig.colorbar(sc)
+    plt.savefig(os.path.join(args_output, sorted_diff_image_name))
+    plt.close()
+
+
+def plot_diff(args_cmap, args_output, diff_image_name, diffs_by_phase, disc_range, vmin, vmax, plot_maxima):
+    # create the trailed spectrum by phase plot
+    fig = plt.figure(figsize=[6.4, 4.8 * 2])
+    plot = fig.add_subplot(111)
+    plot.set_title("measured - predicted")
     plot.set_ylim(-0.5, 1.5)
-    plot.set_xlim(H_ALPHA.value - padding, H_ALPHA.value + padding)
+    plot.set_xlim(disc_range.lower_bound(), disc_range.upper_bound())
     plot.set_ylabel('Phase')
-
-    for diff in diff_plots:
+    for diff in diffs_by_phase:
 
         assert len(diff.wavelength) == len(diff.diff)
 
         for offset in [-1, 0, 1]:
             ys = (diff.phase + offset) * np.ones(len(diff.wavelength))
-            sc = plot.scatter(diff.wavelength, ys, s=1, c=diff.diff, cmap=args.cmap, vmin=min(vmin, -vmax),
+            sc = plot.scatter(diff.wavelength, ys, s=1, c=diff.diff, cmap=args_cmap, vmin=min(vmin, -vmax),
                               vmax=max(vmax, -vmin))
-
     plot.vlines(H_ALPHA.value, *plot.get_ylim())
 
-    fig.colorbar(sc)
+    if plot_maxima:
+        left_xs = []
+        left_ys = []
+        right_xs = []
+        right_ys = []
 
-    plt.savefig(os.path.join(args.output, diff_image_name))
-    plt.close()
+        for diff in diffs_by_phase:
 
-    fig = plt.figure(figsize=[6.4, 4.8 * 2])
-    plot = fig.add_subplot(111)
-    plot.set_title("measured - predicted")
-
-    plot.set_xlim(H_ALPHA.value - padding, H_ALPHA.value + padding)
-    plot.set_ylabel('Spectra sorted by phase')
-
-    for i, diff in enumerate(sorted(diff_plots, key=lambda diff: diff.phase)):
-        assert len(diff.wavelength) == len(diff.diff)
-
-        ys = 1.0 * i * np.ones(len(diff.wavelength))
-        sc = plot.scatter(diff.wavelength, ys, s=1, c=diff.diff, cmap=args.cmap, vmin=min(vmin, -vmax),
-                          vmax=max(vmax, -vmin))
-
-    plot.vlines(H_ALPHA.value, *plot.get_ylim())
+            if 0.15 <= diff.phase <= 0.85:
+                for x, y in diff.maxima:
+                    if x < H_ALPHA.value:
+                        left_xs.append(x)
+                        left_ys.append(diff.phase)
+                    else:
+                        right_xs.append(x)
+                        right_ys.append(diff.phase)
+        plot.plot(left_xs, left_ys, 'k')
+        plot.plot(right_xs, right_ys, 'k')
 
     fig.colorbar(sc)
-
-    plt.savefig(os.path.join(args.output, sorted_diff_image_name))
+    plt.savefig(os.path.join(args_output, diff_image_name))
     plt.close()
+    return sc
 
-    text_file.write("\\end{document}\n")
 
+def create_diff_plot(final_model, initial_model, normalized, maxima, title, xlim, xs, ys, image_path):
 
-def create_diff_plot(final_model, initial_model, normalized, title, xlim, xs, ys, image_path):
+    redshift = final_model.redshift
+
     plot = plt.figure().add_subplot(111)
     plot.set_ylim(-0.5, 1.5)
     plot.set_xlim(xlim)
+
     plot.plot(xs, 0.6 * ys, label='measured')
     plot.plot(xs, normalized, label='normalized')
+
     plot.plot(xs, initial_model(xs), label='predicted %s' % initial_model)
     if final_model is not initial_model:
         plot.plot(xs, final_model(xs), label='fitted %s' % final_model)
+
     plot.plot(xs, normalized - final_model(xs), label='normalized - fitted')
+
+    if maxima:
+        for x, y in maxima:
+            plot.vlines(x + redshift, ymin=0, ymax=y, label='maxima')
+
     plot.hlines(0, xlim[0], xlim[1])
-    plot.vlines(H_ALPHA.value, *plot.get_ylim())
+    plot.vlines(H_ALPHA.value + redshift, *plot.get_ylim())
     plot.set_title(title)
     plot.legend(loc='upper right')
     plt.savefig(image_path)
     plt.close()
+
+
+def _find_maxima(xs, ys, center):
+
+    result = []
+
+    for r in [closed_range(np.min(xs), center), closed_range(center, np.max(xs))]:
+
+        mask = [x in r for x in xs]
+
+        ys_in_r = ys[mask]
+        arg = np.argmax(ys_in_r)
+
+        if arg == 0 or arg + 1 == len(ys_in_r):
+            logger.debug('ignore maximum in %s at %s bound', r, 'lower' if arg == 0 else 'upper')
+            continue
+
+        y = ys_in_r[arg]
+        x = xs[mask][arg]
+
+        logger.debug('maximum in %s at x=%.1f, y=%.2f', r, x, y)
+        result.append((x, y))
+
+    return tuple(result)
 
 
 def _find_minimum(xs, ys, dx, range_AA, box_size_AA):
